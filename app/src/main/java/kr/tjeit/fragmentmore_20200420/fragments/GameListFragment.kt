4a383249ffcff8b8e077936115db98dfa7d1b909
cho881020 <kj_cho@idealidea.co.kr>
2020-04-20 20:12:54 +0900

    프래그먼트에 리스트뷰 배치

--- app/src/main/java/kr/tjeit/fragmentmore_20200420/fragments/GameListFragment.kt
+++ app/src/main/java/kr/tjeit/fragmentmore_20200420/fragments/GameListFragment.kt
@@ -5,9 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_game_list.*
 import kr.tjeit.fragmentmore_20200420.R
+import kr.tjeit.fragmentmore_20200420.adapters.AppAdapter
+import kr.tjeit.fragmentmore_20200420.datas.App
 
 class GameListFragment : Fragment() {
+
+    var mGameAdapter:AppAdapter? = null
+    val mGameList = ArrayList<App>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,7 +26,18 @@ class GameListFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        mGameList.add(App("아스팔트 8 : 에어본", "GameLoft", 5))
+        mGameList.add(App("MineCraft", "Mojang", 4))
+        mGameList.add(App("아스팔트 7 : 히트", "GameLoft", 4))
+        mGameList.add(App("팔라독 (Paladog)", "FazeCat", 2))
+        mGameList.add(App("Plants vs. Zombies", "EA Games", 3))
+        mGameList.add(App("스왐피 (Swampy)", "Disney", 5))
+        mGameList.add(App("리니지2M", "NCSOFT", 3))
+        mGameList.add(App("리니지M", "NCSOFT", 4))
+        mGameList.add(App("AFK아레나", "Lilith Games", 1))
 
+        mGameAdapter = AppAdapter(context!!, R.layout.app_list_item, mGameList)
+        gameListView.adapter = mGameAdapter
 
     }
 
