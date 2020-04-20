934ecd9b9a4e61ae7437b6c716879423d39e1dd7
cho881020 <kj_cho@idealidea.co.kr>
2020-04-20 21:54:38 +0900

    페이지별 제목 설정

--- app/src/main/java/kr/tjeit/fragmentmore_20200420/adapters/MyFragPagerAdapter.kt
+++ app/src/main/java/kr/tjeit/fragmentmore_20200420/adapters/MyFragPagerAdapter.kt
@@ -9,6 +9,14 @@ import kr.tjeit.fragmentmore_20200420.fragments.MainFragment
 
 class MyFragPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position) {
+            0 -> "메인"
+            1 -> "게임 리스트"
+            else -> "거래 게시판"
+        }
+    }
+
     override fun getItem(position: Int): Fragment {
         return when(position) {
             0 -> MainFragment()
