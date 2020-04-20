0e46c379ee97a8b820a535989a206a38bcf74159
cho881020 <kj_cho@idealidea.co.kr>
2020-04-20 19:53:40 +0900

    간단한 이벤트 처리 연습  - 프래그먼트상에서 처리

--- app/src/main/java/kr/tjeit/fragmentmore_20200420/fragments/MainFragment.kt
+++ app/src/main/java/kr/tjeit/fragmentmore_20200420/fragments/MainFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_main.*
 import kr.tjeit.fragmentmore_20200420.R
 
 class MainFragment : Fragment() {
@@ -15,6 +16,20 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//        액티비티의 onCreate에 대응되는 함수.
+        okBtn.setOnClickListener {
+
+            val inputMsg = messageEdt.text.toString()
+
+            contentTxt.text = inputMsg
+
+        }
+
     }
 
 }
