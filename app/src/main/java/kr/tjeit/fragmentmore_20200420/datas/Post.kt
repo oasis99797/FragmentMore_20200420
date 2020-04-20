7d93550244b7d964784ccd6e98873751ed4b45d6
cho881020 <kj_cho@idealidea.co.kr>
2020-04-20 20:42:38 +0900

    거래 게시판 리스트뷰 기초 작업중  - 데이터클래스, XML

---
+++ app/src/main/java/kr/tjeit/fragmentmore_20200420/datas/Post.kt
@@ -0,0 +1,7 @@
+package kr.tjeit.fragmentmore_20200420.datas
+
+import android.util.Log
+
+class Post(var companyName:String, var amount:Int, var userName:String) {
+
+}
---
+++ app/src/main/java/kr/tjeit/fragmentmore_20200420/fragments/BoardFragment.kt
@@ -0,0 +1,7 @@
+package kr.tjeit.fragmentmore_20200420.fragments
+
+import androidx.fragment.app.Fragment
+
+class BoardFragment : Fragment() {
+
+}
