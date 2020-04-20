2b21fcc45ffe7790f32cbd9af13cdcff0055a3e8
cho881020 <kj_cho@idealidea.co.kr>
2020-04-20 21:01:30 +0900

    ViewPager어댑터 클래스 변경  - 3페이지 처리  - return when 문법 활용

--- app/src/main/java/kr/tjeit/fragmentmore_20200420/adapters/MyFragPagerAdapter.kt
+++ app/src/main/java/kr/tjeit/fragmentmore_20200420/adapters/MyFragPagerAdapter.kt
@@ -3,21 +3,21 @@ package kr.tjeit.fragmentmore_20200420.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import kr.tjeit.fragmentmore_20200420.fragments.BoardFragment
 import kr.tjeit.fragmentmore_20200420.fragments.GameListFragment
 import kr.tjeit.fragmentmore_20200420.fragments.MainFragment
 
 class MyFragPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
-        if (position == 0) {
-            return MainFragment()
-        }
-        else {
-            return GameListFragment()
+        return when(position) {
+            0 -> MainFragment()
+            1 -> GameListFragment()
+            else -> BoardFragment()
         }
     }
 
     override fun getCount(): Int {
-        return 2
+        return 3
     }
 }
